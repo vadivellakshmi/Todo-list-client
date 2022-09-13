02db262af915b88cf2c0f16977e882877bc513af
lakshmipriya <lakshmipriya87256@gmail.com>
2022-09-13 15:59:30 +0530

    chnges

--- src/component/Todolist.js
+++ src/component/Todolist.js
@@ -4,52 +4,50 @@ import axios from "axios"
 const Todolist = () =>{
 
     const [list, setList] = useState([])
-    const [addlist, setAddlist] = useState(
-        {listitem:""}
-    )
-
-    
-
-    const getList = () =>{
-        axios.get(" https://todolistmernapp.herokuapp.com/todo/getlist")
-        .then((res) => setList(res.data))
-        .catch(err => console.log(err))
-
-    }
-    const addListItem = (e) =>{
-
-        e.preventDefault()
-          axios.post(' https://todolistmernapp.herokuapp.com/todo/insertlist',addlist)
-          .then((res) => console.log(res.data)
-           )
-           getList()
+    const [listitem, setListitem] = useState("")
         
+   
+        const deleteList = (id) =>{
+
+            axios.delete(" https://todolistmernapp.herokuapp.com/todo/"+id)
+            .then((res) =>{
+                console.log(res)
+            })
+    
+            .catch(err => console.log(err))
         }
-
+    
  
+        useEffect(()=>{
 
 
-    const deleteList = (id) =>{
-
-        axios.delete(" https://todolistmernapp.herokuapp.com/todo/"+id)
-        .then((res) =>{
-            console.log(res)
-            getList()
-        }) 
-
-        .catch(err => console.log(err))
-    }
-
+          
+                axios.get(" https://todolistmernapp.herokuapp.com/todo/getlist")
+                .then((res) => setList(res.data))
+                .catch(err => console.log(err))
+        
+            
+        
     
+        })
+
+   
+        const addListItem = (e) =>{
+
+            e.preventDefault()
+              axios.post('https://todolistmernapp.herokuapp.com/todo/insertlist', {listitem})
+              .then((res) => {console.log(res.data)
+              setListitem("")}
+               )
+              
+               .catch(err => console.log(err))  
+            
+            }
 
 
-    
+  
 
-    useEffect(()=>{
-
-        getList()
-
-    },[])
+  
 
     return(
 <>
@@ -61,11 +59,11 @@ const Todolist = () =>{
             </div>
     </div>
 
-    <form  className="m-auto mt-5 " onSubmit={addListItem}>
+    <form  className="m-auto mt-5 " onSubmit={addListItem} >
         <div className="mb-3 row align-item-center justify-content-center">
             <div className="col-md-6 d-flex ">
-            <input type="text" className="form-control col" placeholder="List The Activity To Do" name="listitem"  value={addlist.listitem}  onChange={(e)=> setAddlist({...addlist, listitem:e.target.value  })}required/>
-            <button className="btn btn-primary ms-3" type="submit">ADD</button></div>
+            <input type="text" className="form-control col" placeholder="List The Activity To Do" name="listitem"  value={listitem}  onChange={(e)=> setListitem(e.target.value)}required/>
+            <button className="btn btn-primary ms-3" type="submit" >ADD</button></div>
            
        
 
